7b57852e229470f43467f365e9973a02eae0a5db
danilodumbacpfl <111308466+danilodumbacpfl@users.noreply.github.com>
2024-05-13 15:54:48 -0300

    Configurando o docker compose

--- src/Infraestructure/Locamoto.Infra.PostgreSql/LocamotoDbFactory.cs
+++ src/Infraestructure/Locamoto.Infra.PostgreSql/LocamotoDbFactory.cs
@@ -4,12 +4,12 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace Locamoto.Infra.PostgreSql;
 
-    public class LocamotoDbFactory: IDesignTimeDbContextFactory<LocamotoEFContext>
+public class LocamotoDbFactory: IDesignTimeDbContextFactory<LocamotoEFContext>
 {
     public LocamotoEFContext CreateDbContext(string[] args)
     {
         var optionBuilder = new DbContextOptionsBuilder<LocamotoEFContext>();
-        optionBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=postgres;Password=123456;Database=locamoto;", a => a.MigrationsAssembly("Locamoto.Infra.PostgreSql"));
+        optionBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=locamoto;Password=123456;Database=locamoto;", a => a.MigrationsAssembly("Locamoto.Infra.PostgreSql"));
 
         return new LocamotoEFContext(optionBuilder.Options);
     }
